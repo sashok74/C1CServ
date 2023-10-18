7f64b84df521f47cca710da34e78e8218a05da94
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-19 01:59:20 +0500

    console.log 3

--- src/controllers/c1_zc.route.ts
+++ src/controllers/c1_zc.route.ts
@@ -4,51 +4,46 @@ import { loadDB } from '../modules/db.js';
 //import { Tposts, Tthemes } from '../types/chatDB.js';
 
 export async function allC1_ZC(req: Request, res: Response) {
-    try {
-        let db = await loadDB();
-        const items = await db.collection('c1_zc').find().toArray();
-        res.send(items);
-    } catch (error) {
-        console.log('error:', error);
-        res.status(500).send(error);
-    }
-};
-
-export async function insertC1_ZC(prm: any) {
-    try {
-        let db = await loadDB();
-        if (!prm._id) {
-            prm._id = new ObjectId();
-            prm.created_at = new Date();
-            prm.updated_at = undefined;
-        } else {
-            prm._id = new ObjectId(prm._id);
-            prm.updated_at = new Date();
-        }
-        prm.p_id = prm.p_id ? new ObjectId(prm.p_id) : null;
-
-
-        let updateObj = {
-            $setOnInsert: {
-                "_id": prm._id,
-                "created_at": prm.created_at
-            },
-            $set: {
-                "updated_at": prm.updated_at,
-            },
-        }
-
-        const uitems = await db.collection('c1_zc').findOneAndUpdate(
-            { "_id": prm._id },
-            updateObj,
-            {
-                upsert: true,
-                returnDocument: 'after'
-            }
-        );
-    } catch (error) {
-        console.log('error:', error);
-    }
+  try {
+    let db = await loadDB();
+    // Коллекция существует, вставляем запись.
+    const items = await db.collection('C1_ZC').find().toArray();
+    res.send(items);
+  } catch (error) {
+    console.log('error:', error);
+    res.status(500).send(error);
+  }
 }
 
+export async function insertC1_ZC(prm: any) {
+  try {
+    let db = await loadDB();
+    if (!prm._id) {
+      prm._id = new ObjectId();
+      prm.created_at = new Date();
+      prm.updated_at = undefined;
+    } else {
+      prm._id = new ObjectId(prm._id);
+      prm.updated_at = new Date();
+    }
+    prm.p_id = prm.p_id ? new ObjectId(prm.p_id) : null;
 
+    let updateObj = {
+      $setOnInsert: {
+        _id: prm._id,
+        created_at: prm.created_at,
+      },
+      $set: {
+        updated_at: prm.updated_at,
+      },
+    };
+
+    const uitems = await db.collection('C1_ZC').findOneAndUpdate({ _id: prm._id }, updateObj, {
+      upsert: true,
+      returnDocument: 'after',
+    });
+    console.log('after:', uitems);
+  } catch (error) {
+    console.log('error:', error);
+  }
+}
