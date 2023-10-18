0414f726cda88c1229b94be74a2093a48dba5ebc
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-19 00:45:24 +0500

    console.log

--- src/modules/1cdata.ts
+++ src/modules/1cdata.ts
@@ -3,11 +3,11 @@ import { insertC1_ZC } from '../controllers/c1_zc.route.js'
 //import { getOrderResponse } from '../testData/res.getDoc.js'
 
 export async function getDoc(uid: string) {
+    console.log("getDoc uid:",uid);
     const doc = await axios.get(`http://192.168.10.183/unf/hs/ht/get_order/${uid}`);
-
+    console.log("axios res:",doc);
     //return response.data; // здесь возвращается JSON-ответ
     insertC1_ZC(doc.data);
-    console.log(uid);
     //return getOrderResponse;
 }
 
