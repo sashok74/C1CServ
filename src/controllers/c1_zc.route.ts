1f2f12c3e3a755af681a4b48d92b73bb12a59c9c
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-19 09:32:24 +0500

    console.log 8

--- src/controllers/c1_zc.route.ts
+++ src/controllers/c1_zc.route.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { ObjectId } from "mongodb";
 import { loadDB } from '../modules/db.js';
 
 
@@ -17,19 +17,15 @@ export async function allC1_ZC(req: Request, res: Response) {
 
 export async function insertC1_ZC(prm: any) {
   try {
+    const today = new Date();
     const db = await loadDB();
-    const uitems = await db.collection('C1_ZC').findOneAndUpdate(
-      { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.ЗаказПокупателя.GUIDЗаказаПокупателя },
-      {
-        $setOnInsert: {...prm, 'res.insert_at': new Date() }, // если документ вставляется
-        $set: { 'res.update_at': new Date() }, // если документ обновляется,
-      },
-      {
-        upsert: false,
-        returnDocument: 'after',
-      },
+    const uitems = await db.collection('C1_ZC').findOne(
+      { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.ЗаказПокупателя.GUIDЗаказаПокупателя }
     );
-    console.log('insert record:', uitems);
+    if (uitems === null){
+      const newrec = await db.collection('C1_ZC').insertOne({...prm, 'res.insert_at': today});
+      console.log('insert record:', newrec);
+    }
   } catch (error) {
     console.log('error:', error);
   }
