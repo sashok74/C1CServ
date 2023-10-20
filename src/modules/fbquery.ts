90efe5ec15e522c882b2935cd0e367b890e68dd6
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-21 00:34:55 +0500

    db_test+

--- src/modules/fbquery.ts
+++ src/modules/fbquery.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const DB_HOST = process.env.DB_HOST;
 export async function db_query(proc: string, trans = 'READ_WRITE',  prm:object) {
     console.log("db_query proc:", proc);
+<<<<<<< HEAD
     const res = await axios.post(`http://${DB_HOST}:3333/query`,
     {
         'procedureName': proc, 
@@ -13,4 +14,14 @@ export async function db_query(proc: string, trans = 'READ_WRITE',  prm:object) 
     }
     );
     return res.data;
+=======
+    const res = await axios.post(`http://${DB_HOST}:3333:/query`,
+    {
+        'procedureName': proc, 
+        'prm':prm
+    }
+    );
+    console.log("axios res:",res.data);
+    //return response.data; // здесь возвращается JSON-ответ
+>>>>>>> 67ed03b (db_test)
 }
