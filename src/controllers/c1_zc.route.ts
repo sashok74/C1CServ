8c0cae1ccedfae7bed91bd1b85bb74eb2b83b665
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-21 17:16:08 +0500

    partner

--- src/controllers/c1_zc.route.ts
+++ src/controllers/c1_zc.route.ts
@@ -22,7 +22,26 @@ export async function insertC1_ZC(prm: any) {
       { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.ЗаказПокупателя.GUIDЗаказаПокупателя }
     );
     if (uitems === null){
-      const newrec = await db.collection('C1_ZC').insertOne({...prm, 'res':{'insert_at': today}});
+      const newrec = await db.collection('C1_ZC').insertOne({...prm, 'res':{'insert_at': today, 'ref_id': null}});
+      console.log('insert record:', newrec);
+    }
+    else{
+      console.log('find record:', uitems);
+    }
+  } catch (error) {
+    console.log('error:', error);
+  }
+}
+
+export async function insertC1_Parner(prm: any) {
+  try {
+    const today = new Date();
+    const db = await loadDB();
+    const uitems = await db.collection('C1_ZC').findOne(
+      { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.Контрагент.GUIDКонтрагента }
+    );
+    if (uitems === null){
+      const newrec = await db.collection('C1_Partner').insertOne({...prm, 'res':{'insert_at': today, 'ref_id': null}});
       console.log('insert record:', newrec);
     }
     else{
--- src/modules/1cdata.ts
+++ src/modules/1cdata.ts
@@ -1,16 +1,28 @@
 import axios from 'axios';
-import { insertC1_ZC } from '../controllers/c1_zc.route.js'
+import { insertC1_ZC, insertC1_Parner } from '../controllers/c1_zc.route.js'
 //import { getOrderResponse } from '../testData/res.getDoc.js'
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const C1_WEBSERVER = process.env.C1_WEBSERVER;
+
 export async function getDoc(uid: string) {
     console.log("getDoc uid:",uid);
-    const doc = await axios.get(`http://${C1_WEBSERVER}/unf/hs/ht/get_order/${uid}`);
-    console.log("axios res:",doc.data);
+    const res = await axios.get(`http://${C1_WEBSERVER}/unf/hs/ht/get_order/${uid}`);
+    console.log("axios res:",res.data);
     //return response.data; // здесь возвращается JSON-ответ
-    await insertC1_ZC(doc.data);
+    //сперва проверим все вложеные объекты
+    //контрагент.
+    await getPartner(res.data.response.ЗаказПокупателя.КонтрагентЗаказаПокупателя.GUIDКонтрагента);
+    await insertC1_ZC(res.data);
     //return getOrderResponse;
 }
 
+export async function getPartner(uid: string) {
+    console.log("getDoc uid:",uid);
+    const res = await axios.get(`http://${C1_WEBSERVER}/unf/hs/ht/get_partner/${uid}`);
+    console.log("axios res:",res.data);
+    //return response.data; // здесь возвращается JSON-ответ
+    await insertC1_Parner(res.data);
+    //return getOrderResponse;
+}
