9ae68a87ca2601eb7d9349c2b41b88b713b706d8
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-22 19:48:16 +0500

    rebase

--- src/controllers/c1_zc.route.ts
+++ src/controllers/c1_zc.route.ts
@@ -14,45 +14,6 @@ export async function allC1_ZC(req: Request, res: Response) {
   }
 }
 
-<<<<<<< HEAD
 export async function GetCollectionDriver (collectionName: string, queryField: string) {
   return new MongoDBCollection(collectionName, queryField);
-=======
-export async function insertC1_ZC(prm: any) {
-  try {
-    const today = new Date();
-    const db = await loadDB();
-    const uitems = await db.collection('C1_ZC').findOne(
-      { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.ЗаказПокупателя.GUIDЗаказаПокупателя }
-    );
-    if (uitems === null){
-      const newrec = await db.collection('C1_ZC').insertOne({...prm, 'res':{'insert_at': today, 'ref_id': null}});
-      console.log('insert record:', newrec);
-    }
-    else{
-      console.log('find record:', uitems);
-    }
-  } catch (error) {
-    console.log('error:', error);
-  }
-}
-
-export async function insertC1_Parner(prm: any) {
-  try {
-    const today = new Date();
-    const db = await loadDB();
-    const uitems = await db.collection('C1_ZC').findOne(
-      { 'response.ЗаказПокупателя.GUIDЗаказаПокупателя': prm.response.Контрагент.GUIDКонтрагента }
-    );
-    if (uitems === null){
-      const newrec = await db.collection('C1_Partner').insertOne({...prm, 'res':{'insert_at': today, 'ref_id': null}});
-      console.log('insert record:', newrec);
-    }
-    else{
-      console.log('find record:', uitems);
-    }
-  } catch (error) {
-    console.log('error:', error);
-  }
->>>>>>> 8c0cae1 (partner)
 }
