6a24f1b1338c17faf2469e95dd19028a103a57d5
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-23 10:03:57 +0500

    common collection hendler

---
+++ src/types/ExportSchemes.ts
@@ -0,0 +1,43 @@
+import { ObjProcessFunc, ObjectSchemType } from './C1Types.js';
+
+// prmMap соответсвеие параметров процедуры для экспорта и 
+// поелй объекта и которого эти данные берем
+// получить параметры процедуры можно из функции MET$PROC_IN_PARAM_INFO_S
+
+export const ZakazClienta: ObjectSchemType = {
+  schemeName: 'Заказ клиента',
+  collectionName: 'C1_ZC',
+  queryField: 'response.ЗаказПокупателя.GUIDЗаказаПокупателя',
+  servC1Path: 'get_order',
+  exportProcName: 'c1_zakaz_h_i',
+  prmMap: {
+    NUM: '',
+    DATA_Z: '',
+    SROK_Z: '',
+    KOD_ZAK: '',
+    NAME_ZAK: '',
+    NUM_ZAIY: '',
+    DATA_ZAIY: '',
+  },
+  idField: 'res_id',
+  procFn: (obj) => {},
+};
+
+export const Kontragent: ObjectSchemType = {
+    schemeName: 'Контрагент',
+    collectionName: 'C1_Partner',
+    queryField: 'response.Контрагент.GUIDКонтрагента',
+    servC1Path: 'get_partner',
+    exportProcName: 'c1_zakaz_h_i',
+    prmMap: {
+      NUM: '',
+      DATA_Z: '',
+      SROK_Z: '',
+      KOD_ZAK: '',
+      NAME_ZAK: '',
+      NUM_ZAIY: '',
+      DATA_ZAIY: '',
+    },
+    idField: 'res_id',
+    procFn: (obj) => {},
+  };
