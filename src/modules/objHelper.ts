78c3bedfe3dac0e3a0bf7b786df105812adf9faf
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-31 08:07:21 +0500

    mod getObjectC1

--- src/modules/objHelper.ts
+++ src/modules/objHelper.ts
@@ -1,5 +1,5 @@
 export function getValueByPath(obj: any, path: string) {
-  if (path.trim.length === 0 ) return obj;
+  if (path === '' ) return obj;
   return path.split('.').reduce((prev, curr) => {
     return (prev && prev[curr] !== undefined && prev[curr] !== null) ? prev[curr] : undefined;
 }, obj);
