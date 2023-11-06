1e82ad5fa2d98a33ef95ce3dfa55892b71d7fa21
Makarenko Aleksandr <rolliks@gmail.com>
2023-11-07 00:11:05 +0500

    add NomParse

--- src/types/C1NomPaterns.ts
+++ src/types/C1NomPaterns.ts
@@ -2,7 +2,7 @@ export const regexPatterns = [
     {
       name: 'Конденсатор чип CC',
       params: [
-        { name: 'параметр1', pattern: /\d+(,\d+)?(pF|uF)/i },
+        { name: 'параметр1', pattern: /\d+(,\d+)?[a-zA-Z]+F/i },
         { name: 'параметр2', pattern: /[±]\d+%/i },
         { name: 'параметр3', pattern: /\d+v/i },
         { name: 'параметр4', pattern: /X\d+R|NPO|NP0/i },
