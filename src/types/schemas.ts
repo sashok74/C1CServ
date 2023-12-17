92073e459d21df7ce6f001f42dddcac55fe470e2
sashok74 <rolliks@gmail.com>
2023-12-17 13:20:36 +0500

    add shema for validate

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -1,9 +1,23 @@
-export const nomSchema = {
-    type: 'array',
-    items: { type: 'number' }
+  export const nomSchema = {
+    type: 'object',
+    properties: {
+      NOM: {
+        type: 'array',
+        items: { type: 'number' }
+      }
+    },
+    required: ['NOM'],
+    additionalProperties: false // не допускать дополнительных свойств
   };
 
   export const docSchema = {
-    type: 'array',
-    items: { type: 'string' }
+    type: 'object',
+    properties: {
+      DOC: {
+        type: 'array',
+        items: { type: 'string' }
+      }
+    },
+    required: ['DOC'],
+    additionalProperties: false // не допускать дополнительных свойств
   };
